855dcca8b0ec2753b07df52b4f92753175a5f0ae
mtsarah <bers@xxxlgroup.com>
2021-11-06 19:06:19 +0100

    add splash screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Splash from "./components/Splash/Splash.js";
 
 function App() {
   return (
-    <div className="">
-      <header className="text-5xl">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen">
+      <Splash />
     </div>
   );
 }

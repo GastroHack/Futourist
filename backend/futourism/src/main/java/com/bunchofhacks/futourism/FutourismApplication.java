25002d8b54eb40b38dfba3522b478309642e4e90
KasnocknDave <dhn@xxxlgroup.com>
2021-11-06 16:51:36 +0100

    add project setup

---
+++ backend/futourism/src/main/java/com/bunchofhacks/futourism/FutourismApplication.java
@@ -0,0 +1,13 @@
+package com.bunchofhacks.futourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutourismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutourismApplication.class, args);
+	}
+
+}

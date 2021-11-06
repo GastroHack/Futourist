5f17817f1d46f32db17c5dd468c9228cd2309f43
Prince Sodhi <princesodhi@hotmail.com>
2021-11-06 19:09:40 +0100

    chore: add overview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import Splash from "./components/Splash/Splash.js";
+import Overview from "./components/Overview";
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Splash />
+    <div className="">
+      <header className="text-2xl">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Overview />
     </div>
   );
 }

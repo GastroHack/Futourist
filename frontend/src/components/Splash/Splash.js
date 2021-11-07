0ca0483994a714b12b8e84267afba439c89b0e57
Katharina Fluch <katharina.fluch@gmx.at>
2021-11-07 13:13:50 +0100

    add styling for logo

--- frontend/src/components/Splash/Splash.js
+++ frontend/src/components/Splash/Splash.js
@@ -5,7 +5,7 @@ import "./Splash.css";
 
 const Splash = () => (
   <div className="flex flex-col justify-center h-screen mx-24">
-    <Logo />
+    <Logo className="w-full" />
     <Link to="/form" className="flex mx-auto mt-12">
       <button className="startJourney px-5 w-auto whitespace-nowrap">
         Start your Adventure!

cb3fcc802922f776d776832c731631e415fd30fc
Prince Sodhi <princesodhi@hotmail.com>
2021-11-07 10:51:01 +0100

    chore: fix console warnings

--- frontend/src/components/Form/components/GeneralInfo/GeneralInfo.js
+++ frontend/src/components/Form/components/GeneralInfo/GeneralInfo.js
@@ -17,7 +17,7 @@ const GeneralInfo = () => {
         <input
           id="location"
           type="text"
-          value={location}
+          defaultValue={location}
           className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
         />
       </div>

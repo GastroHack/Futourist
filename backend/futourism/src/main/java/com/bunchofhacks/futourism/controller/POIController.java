20bc93b4dce7c9e8fdeb03c2e533e978da254ab2
KasnocknDave <dhn@xxxlgroup.com>
2021-11-07 13:38:52 +0100

    refactor controller code

--- backend/futourism/src/main/java/com/bunchofhacks/futourism/controller/POIController.java
+++ backend/futourism/src/main/java/com/bunchofhacks/futourism/controller/POIController.java
@@ -17,6 +17,8 @@ import java.util.List;
 @RestController
 public class POIController {
 
+  private static final String errorResponse = "No data available.";
+
   @Autowired private ApplicationContext applicationContext;
 
   @GetMapping(path = "/scenario1", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -36,22 +38,14 @@ public class POIController {
 
     List<List<POIOutput>> completeOutputList = new ArrayList<>();
 
-    for ( List<Item> itemList : outputList ){
-      List<POIOutput> poiOutputList = new ArrayList<>();
-
-      for ( Item singleItem : itemList ){
-        poiOutputList.add(POIOutputMapper.map(singleItem));
-      }
-
-      completeOutputList.add(poiOutputList);
-    }
+    mapOutputList(outputList, completeOutputList);
 
     try {
       return mapper.writeValueAsString(completeOutputList);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
-    return "Error";
+    return errorResponse;
   }
 
   @GetMapping(path = "/scenario2", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -70,22 +64,14 @@ public class POIController {
 
     List<List<POIOutput>> completeOutputList = new ArrayList<>();
 
-    for ( List<Item> itemList : outputList ){
-      List<POIOutput> poiOutputList = new ArrayList<>();
-
-      for ( Item singleItem : itemList ){
-        poiOutputList.add(POIOutputMapper.map(singleItem));
-      }
-
-      completeOutputList.add(poiOutputList);
-    }
+    mapOutputList(outputList, completeOutputList);
 
     try {
       return mapper.writeValueAsString(completeOutputList);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
-    return "Error";
+    return errorResponse;
   }
 
   @GetMapping(path = "/scenario3", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -94,32 +80,24 @@ public class POIController {
     ObjectMapper mapper = new ObjectMapper();
 
     List<List<Item>> outputList =
-            applicationContext
-                    .getBean(POIDataProvider.class)
-                    .getItemsbyIds(
-                            "6fbb10c6-53fe-4b90-9a09-1061f1a73ada",
-                            "a5896801-537a-43fb-bbfd-013d05b1e451",
-                            "422311a7-a239-4ccd-a4d2-0bdcead9f91d",
-                            "ecb7da25-c535-41a2-accf-da07b16149ec\n");
+        applicationContext
+            .getBean(POIDataProvider.class)
+            .getItemsbyIds(
+                "6fbb10c6-53fe-4b90-9a09-1061f1a73ada",
+                "a5896801-537a-43fb-bbfd-013d05b1e451",
+                "422311a7-a239-4ccd-a4d2-0bdcead9f91d",
+                "ecb7da25-c535-41a2-accf-da07b16149ec\n");
 
     List<List<POIOutput>> completeOutputList = new ArrayList<>();
 
-    for ( List<Item> itemList : outputList ){
-      List<POIOutput> poiOutputList = new ArrayList<>();
-
-      for ( Item singleItem : itemList ){
-        poiOutputList.add(POIOutputMapper.map(singleItem));
-      }
-
-      completeOutputList.add(poiOutputList);
-    }
+    mapOutputList(outputList, completeOutputList);
 
     try {
       return mapper.writeValueAsString(completeOutputList);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
-    return "Error";
+    return errorResponse;
   }
 
   @GetMapping(path = "/scenario4", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -128,33 +106,36 @@ public class POIController {
     ObjectMapper mapper = new ObjectMapper();
 
     List<List<Item>> outputList =
-            applicationContext
-                    .getBean(POIDataProvider.class)
-                    .getItemsbyIds(
-                            "1ccabb8c-574b-44f0-8fb7-30fee98d8eef",
-                            "1ced8a81-d613-4fc8-9180-046e6462e398",
-                            "9661c65a-950b-4e9f-88f8-83d607f81e40",
-                            "f2834416-584d-4224-869e-9f15d7fc73fa\n");
+        applicationContext
+            .getBean(POIDataProvider.class)
+            .getItemsbyIds(
+                "1ccabb8c-574b-44f0-8fb7-30fee98d8eef",
+                "1ced8a81-d613-4fc8-9180-046e6462e398",
+                "9661c65a-950b-4e9f-88f8-83d607f81e40",
+                "f2834416-584d-4224-869e-9f15d7fc73fa\n");
 
     List<List<POIOutput>> completeOutputList = new ArrayList<>();
 
-    for ( List<Item> itemList : outputList ){
-      List<POIOutput> poiOutputList = new ArrayList<>();
-
-      for ( Item singleItem : itemList ){
-        poiOutputList.add(POIOutputMapper.map(singleItem));
-      }
-
-      completeOutputList.add(poiOutputList);
-    }
+    mapOutputList(outputList, completeOutputList);
 
     try {
       return mapper.writeValueAsString(completeOutputList);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
-    return "Error";
+    return errorResponse;
   }
 
+  private void mapOutputList(
+      List<List<Item>> outputList, List<List<POIOutput>> completeOutputList) {
+    for (List<Item> itemList : outputList) {
+      List<POIOutput> poiOutputList = new ArrayList<>();
 
+      for (Item singleItem : itemList) {
+        poiOutputList.add(POIOutputMapper.map(singleItem));
+      }
+
+      completeOutputList.add(poiOutputList);
+    }
+  }
 }

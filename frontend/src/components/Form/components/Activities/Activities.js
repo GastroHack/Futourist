84b90fc3f48c614b7d1622287235395603d35f19
mtsarah <bers@xxxlgroup.com>
2021-11-07 10:00:41 +0100

    activities

--- frontend/src/components/Form/components/Activities/Activities.js
+++ frontend/src/components/Form/components/Activities/Activities.js
@@ -3,38 +3,38 @@ import { Link } from "react-router-dom";
 import "./Activities.css";
 
 const Activities = () => {
-  return <div>Placeholder for Activities</div>;
-  //   const types = ["diamond", "piggy", "family", "compass"];
-  //   return (
-  //     <div className="typeWrapper">
-  //       <div className="mb-10">
-  //         <label htmlFor="location">Type of Travel</label>
-  //       </div>
+  //   return <div>Placeholder for Activities</div>;
+  const activities = ["museum", "camera", "mountain", "mask"];
+  return (
+    <div className="typeWrapper">
+      <div className="mb-10">
+        <label htmlFor="location">Activities</label>
+      </div>
 
-  //       <div>
-  //         <div className="typeButtons">
-  //           {types.map((element) => (
-  //             <div>
-  //               <button className={`typeButton ${element}`}>
-  //                 <input
-  //                   type="radio"
-  //                   id={`${element}`}
-  //                   name="transportation"
-  //                   value={`${element}`}
-  //                   className="radio"
-  //                 />
-  //               </button>
-  //             </div>
-  //           ))}
-  //         </div>
-  //       </div>
-  //       <div className="buttonWrapper">
-  //         <button className="next">
-  //           <Link to="/activities">NEXT</Link>
-  //         </button>
-  //       </div>
-  //     </div>
-  //   );
+      <div>
+        <div className="typeButtons">
+          {activities.map((element) => (
+            <div>
+              <button className={`typeButton ${element}`}>
+                <input
+                  type="radio"
+                  id={`${element}`}
+                  name="transportation"
+                  value={`${element}`}
+                  className="radio"
+                />
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="buttonWrapper">
+        <button className="next">
+          <Link to="/activities">NEXT</Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Activities;
